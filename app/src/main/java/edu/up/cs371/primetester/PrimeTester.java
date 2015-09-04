aa58266993d7a7710afca8d85de99f03dac5c339
AWilliams18 <williaan18>
2015-09-03 17:04:45 -0700

    "Fixed Tester."

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -14,6 +14,9 @@ public class PrimeTester {
      * @return true iff n is prime
      */
     public static boolean isPrime(long n) {
+        if(n < 2) {
+            return false;
+        }
         for(int i = 2; i <= n/2; i++) {
             if(n%i == 0) {
                 return false;
